8401da7f98e3da803ae42071a4830ef67345445f
s4lieri <54909653+s4lieri-the-f@users.noreply.github.com>
2023-07-14 12:18:42 +0300

    Initial Commit
    A.L.I.C.E prever.2

---
+++ main.py
@@ -0,0 +1,20 @@
+﻿from client import Client
+
+def main():
+    client = Client()
+    log = \
+f"""
+    INFO:
+        Name: {client.name}
+
+    VK:
+        API ver.: {client.vk_ver}
+        Credentials: {client.logon}:{client.passw}
+        Token: {client.token}
+
+    EXTENSIONS VARIABLES:"""
+    for key in client.ext.keys():
+            log += "\n\t\t" + key.upper().replace('_', ' ').replace('api', 'API') + ": " + client.ext[key]
+    client.log('info', log)
+
+main()
