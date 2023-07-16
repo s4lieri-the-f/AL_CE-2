d77ded625bfed482cd8d3c0650e096b5017e92e0
s4lieri <aprasidze.gera@gmail.com>
2023-07-16 19:13:27 +0300

    Pre-release commit (thank u pasta) of ALICE rework

--- client.py
+++ client.py
@@ -4,138 +4,163 @@ import configparser
 import os
 from datetime import datetime as dt
 
+
 class Client:
     def __init__(self) -> None:
-        #reading config contents, checking if does work
+        # reading config contents, checking if does work
         config = configparser.ConfigParser()
-        config.read('assets/config.ini')
-        self.name = config['INFO']['name']
-        self.vk_ver = config['VK']['vk_ver']
-        self.album_id = config['VK']['album_id']
-        self.logon = config['VK']['login']
-        self.passw = config['VK']['passw']
-        self.token = config['VK']['token']
+        config.read("assets/config.ini")
+        self.name = config["INFO"]["name"]
+        self.vk_ver = config["VK"]["vk_ver"]
+        self.album_id = config["VK"]["album_id"]
+        self.logon = config["VK"]["login"]
+        self.passw = config["VK"]["passw"]
+        self.token = config["VK"]["token"]
 
-        #loading extension variables for CAME
-        self.ext = config['EXTENSIONS']
+        # loading extension variables for CAME
+        self.ext = config["EXTENSIONS"]
 
-        #opening a new .log file, writing into it first line, then use for it log() method
-        if 'current.log' in os.listdir('logs'):
-            os.rename('logs/current.log', f'logs/prev{len(os.listdir("logs"))}.log')
-        with open('logs/current.log', 'w') as FILE:
-            FILE.write(f'[{dt.now()} | INFO] Starting {self.name} instance via external call.')
+        # opening a new .log file, writing into it first line, then use for it log() method
+        if "current.log" in os.listdir("logs"):
+            os.rename("logs/current.log", f'logs/prev{len(os.listdir("logs"))}.log')
+        with open("logs/current.log", "w") as FILE:
+            FILE.write(
+                f"[{dt.now()} | INFO] Starting {self.name} instance via external call."
+            )
 
-        #check if token is expired or incorrect
-        if 'error' in self.get_self().keys():
-            self.log('error',
-                    f'''Token is incorrect or expired, trying to get new.
+        # check if token is expired or incorrect
+        if "error" in self.get_self().keys():
+            self.log(
+                "error",
+                f"""Token is incorrect or expired, trying to get new.
                             Login: {self.logon}
-                            Password: {self.passw}'''
-                    )
-            
-            #getting new using credentials from setup
+                            Password: {self.passw}""",
+            )
+
+            # getting new using credentials from setup
             self.token = self.login()
-            config['VK']['token'] = self.token
-            config.write(open('assets/key/config.ini', 'w'))
+            config["VK"]["token"] = self.token
+            config.write(open("assets/key/config.ini", "w"))
 
     def login(self) -> str:
-        #Connecting to VK bot, logging in using
-        token = loads(requests.get(
-            f"https://oauth.vk.com/token?grant_type=password", 
+        # Connecting to VK bot, logging in using
+        token = loads(
+            requests.get(
+                f"https://oauth.vk.com/token?grant_type=password",
                 {
-                    'grant_type': 'password',
-                    'client_id': 2274003,
-                    'client_secret': 'hHbZxrka2uZ6jB1inYsH',
-                    'username': self.logon,
-                    'password': self.passw,
-                    'v': '5.131'
-                }).content)
-        
-        if 'access_token' not in token.keys():
-            self.log('fatal error', f"{token['error']['error_msg']}")
+                    "grant_type": "password",
+                    "client_id": 2274003,
+                    "client_secret": "hHbZxrka2uZ6jB1inYsH",
+                    "username": self.logon,
+                    "password": self.passw,
+                    "v": "5.131",
+                },
+            ).content
+        )
 
-        return token['access_token']
+        if "access_token" not in token.keys():
+            self.log("fatal error", f"{token['error']['error_msg']}")
+
+        return token["access_token"]
 
     def refresh(self, chats: list) -> list:
         link = "https://api.vk.com/method/messages.getHistory"
 
-        messages=[]
+        messages = []
         for chat in chats:
-            data = {'peer_id': chat,
-                    'count': 10,
-                    'v': self.vk_ver,
-                    'access_token': self.token}
+            data = {
+                "peer_id": chat,
+                "count": 10,
+                "v": self.vk_ver,
+                "access_token": self.token,
+            }
             messages.append(loads(requests.post(link, data=data).content))
-        
+
         return messages
 
     def send_message(self, message: str, where: int, image=None) -> None:
         link = "https://api.vk.com/method/messages.send"
         data = {
-                    'peer_id': where,
-                    'message': message,
-                    'random_id': 0,
-                    'access_token': self.token,
-                    'v': self.vk_ver
-                } 
-        
-        if image: data['attachment'] = self.save_image(image)
+            "peer_id": where,
+            "message": message,
+            "random_id": 0,
+            "access_token": self.token,
+            "v": self.vk_ver,
+        }
+
+        if image:
+            data["attachment"] = self.save_image(image)
         requests.post(link, data=data)
-        self.log('info', f'Successfully sent message to {where} conf.')
-        
-    def save_image(self, image: str) -> str:
+        self.log("info", f"Successfully sent message to {where} conf.")
+
+    def save_image(self, path_to_image: str) -> str:
         link = "https://api.vk.com/method/photos.getUploadServer"
-        data = {
-                    'album_id': self.album_id,
-                    'access_token': self.token,
-                    'v': self.vk_ver
-                }
-        
-        upload_link = loads(requests.post(link, data=data).content)['response']['upload_url']
-        to_save = loads(requests.post(upload_link, files={'file1':open(image, 'rb')}).content)
+        data = {"album_id": self.album_id, "access_token": self.token, "v": self.vk_ver}
+
+        upload_link = loads(requests.post(link, data=data).content)["response"][
+            "upload_url"
+        ]
+        to_save = loads(
+            requests.post(
+                upload_link, files={"file1": open(path_to_image, "rb")}
+            ).content
+        )
 
         link = "https://api.vk.com/method/photos.save"
         data = {
-                    'album_id': self.album_id,
-                    'server': to_save['server'],
-                    'photos_list': to_save['photos_list'],
-                    'hash': to_save['hash'],
-                    'aid': to_save['aid'],
-                    'access_token': self.token,
-                    'v': self.vk_ver
-                }
-        
-        photo_id = loads(requests.post(link, data).content)
-        self.log('info', f"Uploaded an image, id: {'photo' + str(photo_id['response'][0]['owner_id']) + '_' + str(photo_id['response'][0]['id'])}")
-        return 'photo' + str(photo_id['response'][0]['owner_id']) + '_' + str(photo_id['response'][0]['id'])
+            "album_id": self.album_id,
+            "server": to_save["server"],
+            "photos_list": to_save["photos_list"],
+            "hash": to_save["hash"],
+            "aid": to_save["aid"],
+            "access_token": self.token,
+            "v": self.vk_ver,
+        }
 
-    def wall_post(self, id: int, text: str) -> str:
+        photo_id = loads(requests.post(link, data).content)
+        self.log(
+            "info",
+            f"Uploaded an image, id: {'photo' + str(photo_id['response'][0]['owner_id']) + '_' + str(photo_id['response'][0]['id'])}",
+        )
+        return (
+            "photo"
+            + str(photo_id["response"][0]["owner_id"])
+            + "_"
+            + str(photo_id["response"][0]["id"])
+        )
+
+    def wall_post(self, id: int, text: str, path_to_img: str) -> str:
         link = "https://api.vk.com/method/wall.post"
         data = {
-                    'owner_id': id,
-                    'attachments': 'photo-205064346_457239018',
-                    'from_group': 1,
-                    'message': text,
-                    'access_token': self.token,
-                    'v': self.vk_ver
-                }
+            "owner_id": id,
+            "attachments": self.save_image(path_to_img),
+            "from_group": 1,
+            "message": text,
+            "access_token": self.token,
+            "v": self.vk_ver,
+        }
 
         result = loads(requests.post(link, data=data).content)
-        self.log('info', f"Posted here: {'https://vk.com/w='+'-'+str(id)+'_'+str(result['response']['post_id'])}")
-        return 'https://vk.com/w='+'-'+str(id)+'_'+str(result['response']['post_id'])
-    
-    
+        self.log(
+            "info",
+            f"Posted here: {'https://vk.com/w='+'-'+str(id)+'_'+str(result['response']['post_id'])}",
+        )
+        return (
+            "https://vk.com/w="
+            + "-"
+            + str(id)
+            + "_"
+            + str(result["response"]["post_id"])
+        )
+
     def get_self(self) -> str:
         link = "https://api.vk.com/method/account.getProfileInfo"
-        data = {
-                    'access_token': self.token,
-                    'v': self.vk_ver
-                }       
-        
-        return loads(requests.post(link, data=data).content)
-    
-    def log(self, t: str, e: str) -> None:
-        with open(f'logs/current.log', 'a') as FILE:
-            FILE.write(f'\n[{dt.now()} | {t.upper()}] '+e)
+        data = {"access_token": self.token, "v": self.vk_ver}
 
-    #def roll():
+        return loads(requests.post(link, data=data).content)
+
+    def log(self, t: str, e: str) -> None:
+        with open(f"logs/current.log", "a") as FILE:
+            FILE.write(f"\n[{dt.now()} | {t.upper()}] " + e)
+
+    # def roll():
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 ﻿from client import Client
+import os
+import importlib
+
 
 def main():
     client = Client()
-    log = \
-f"""
+    log = f"""
     INFO:
         Name: {client.name}
 
@@ -14,7 +16,22 @@ f"""
 
     EXTENSIONS VARIABLES:"""
     for key in client.ext.keys():
-            log += "\n\t\t" + key.upper().replace('_', ' ').replace('api', 'API') + ": " + client.ext[key]
-    client.log('info', log)
+        log += (
+            "\n\t\t"
+            + key.capitalize().replace("_", " ").replace("api", "API")
+            + ": "
+            + client.ext[key]
+        )
+    client.log("info", log)
+
+    handlers = [x[:-3] for x in os.listdir("handlers") if x != "__pycache__"]
+    client.log(
+        "info",
+        f"There are {len(handlers)} handlers in the /handlers folder. \n\t\tThey are: {', '.join(handlers)}.\n\t\tNow will check if conditions are colliding.",
+    )
+
+    for handler in handlers:
+        globals()[handler] = importlib.import_module("handlers." + handler)
+
 
 main()
