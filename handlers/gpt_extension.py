a4fa44919d5d5233194c965658e1b9f2863fdb9d
s4lieri <aprasidze.gera@gmail.com>
2023-07-16 19:15:53 +0300

    forgot to add some files to prev commit

---
+++ handlers/gpt_extension.py
@@ -0,0 +1,11 @@
+from client import Client
+from extensions.gpt import *
+
+# Mandatory variables list
+EXAMPLE_ID = "example"
+COMMAND = "example"
+
+
+# Function, using Client() to return requested result.
+def handle(client):
+    return
