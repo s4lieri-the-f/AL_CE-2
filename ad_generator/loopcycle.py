7a96d852010893317f2cc7d2c2ac27d54498079c
s4lieri <aprasidze.gera@gmail.com>
2023-10-07 03:35:28 +0300

    Loop of ad, need now to change image sending

---
+++ ad_generator/loopcycle.py
@@ -0,0 +1,70 @@
+import random
+
+import vksender
+import ad_generator
+from datetime import datetime, timedelta
+from time import sleep
+from rich import print
+
+log_id = 2000000014
+
+# init
+#alice = vksender.SimpleVKClient(login="karuizawalice@gmail.com", password="|Dat4Lymb0#|")
+
+# теперь инициализировать всех платников муахаха
+#alice.send_message(f"[{datetime.now()}] Main loopcycle initiated, now fetching data from DB, then waiting for midnight.", log_id)
+ad = ad_generator.Ad("user_ad_groups.db3")
+lifecycle_users = {}
+users = ad.get_all_users()
+for user in users:
+    lifecycle_users[user[0]] = {'group': ad.get_user_group_id(user[0]),
+                                'ad_groups': {key: [value[0][4]-12, value[0][4]] for key, value in [(x, ad.get_ad_group(x)) for x in ad.get_ad_groups(ad.get_user_group_id(user[0]))]}}
+print(lifecycle_users)
+
+#alice.send_message(f"[{datetime.now()}] Initiated {len(lifecycle_users)} users.", log_id)
+
+# уебищное говно, но лучше стандартизировать
+# Дожидаемся полуночи и начинаем ХУЯРИТЬ
+ct = datetime.now()
+midnight = ct.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
+time_left = midnight - ct
+
+# sleep(time_left.total_seconds())
+
+while True:
+    users = ad.get_all_users()
+    if len(users) != len(lifecycle_users.keys()):
+        to_delete = [x for x in lifecycle_users.keys() if x not in users]
+        to_add = [x for x in users if x not in lifecycle_users.keys()]
+        for x in to_delete:
+            lifecycle_users.pop(x)
+        for x in to_add:
+            lifecycle_users[x] = {'group': ad.get_user_group_id(x[0]),
+                                        'ad_groups': {key: [value[0][4] - 12, value[0][4]] for key, value in
+                                                      [(x, ad.get_ad_group(x)) for x in
+                                                       ad.get_ad_groups(ad.get_user_group_id(x[0]))]}}
+
+    for group in lifecycle_users.keys():
+
+        gr = lifecycle_users[group]
+        if len(gr["ad_groups"].keys()) != ad.get_ad_groups(gr["group"]):
+            to_delete = [x for x in gr["ad_groups"].keys() if x not in ad.get_ad_groups(gr["group"])]
+            to_add = [x for x in ad.get_ad_groups(gr["group"]) if x not in gr["ad_groups"].keys()]
+            for x in to_delete:
+                gr["ad_groups"].pop(x)
+            for x in to_add:
+                gr["ad_groups"][x] = [ad.get_ad_group(x)[0][4] - 12, ad.get_ad_group(x)[0][4]]
+
+        for ad_gr in gr["ad_groups"].keys():
+            ad_group = gr["ad_groups"][ad_gr]
+            ad_group[0] += 12
+            if ad_group[0] == ad_group[1]:
+                link, period, post, image = ad.assemble(gr['group'], ad_gr, random.choice(ad.get_ad_post_ids(gr['group'])))
+                print(link)
+
+    print(lifecycle_users)
+    ct = datetime.now()
+    midnight = ct.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(hours=12)
+    sleep((midnight - ct).total_seconds())
+
+
--- ad_generator/vksender.py
+++ ad_generator/vksender.py
@@ -1,4 +1,5 @@
 import requests
+from json import loads
 
 class SimpleVKClient:
     def __init__(self, login: str, password: str, vk_ver: str = "5.131"):
@@ -39,7 +40,6 @@ class SimpleVKClient:
             response = requests.get("https://oauth.vk.com/token?grant_type=password", params=params)
             token_data = response.json()
             print(token_data)
-        print(token_data)
         return token_data.get("access_token", "")
 
     def send_message(self, message: str, peer_id: int) -> None:
@@ -54,6 +54,55 @@ class SimpleVKClient:
         response = requests.post(link, data=data)
         # Если нужно обработать ответ от VK API, делаем это тут
 
-if __name__ == "__main__":
-    client = SimpleVKClient(login="Aprasidze.gera@gmail.com", password="2z5l6WhU7z!")
-    client.send_message(message="тест бота", peer_id=166592935)
+    def save_image(self, path_to_image: str) -> str:
+        link = "https://api.vk.com/method/photos.getUploadServer"
+        data = {"album_id": self.album_id, "access_token": self.token, "v": self.vk_ver}
+
+        upload_link = loads(requests.post(link, data=data).content)["response"][
+            "upload_url"
+        ]
+        to_save = loads(
+            requests.post(
+                upload_link, files={"file1": open(path_to_image, "rb")}
+            ).content
+        )
+
+        link = "https://api.vk.com/method/photos.save"
+        data = {
+            "album_id": self.album_id,
+            "server": to_save["server"],
+            "photos_list": to_save["photos_list"],
+            "hash": to_save["hash"],
+            "aid": to_save["aid"],
+            "access_token": self.token,
+            "v": self.vk_ver,
+        }
+
+        photo_id = loads(requests.post(link, data).content)
+        return (
+            "photo"
+            + str(photo_id["response"][0]["owner_id"])
+            + "_"
+            + str(photo_id["response"][0]["id"])
+        )
+
+    def wall_post(self, id: int, text: str, path_to_img: str) -> str:
+        link = "https://api.vk.com/method/wall.post"
+        data = {
+            "owner_id": id,
+            "attachments": self.save_image(path_to_img),
+            "from_group": 1,
+            "message": text,
+            "access_token": self.token,
+            "v": self.vk_ver,
+        }
+
+        result = loads(requests.post(link, data=data).content)
+        return (
+            "https://vk.com/w="
+            + "-"
+            + str(id)
+            + "_"
+            + str(result["response"]["post_id"])
+        )
+
