f41b5de8359e7b704d7400c9d5135e7d3195e299
s4lieri <aprasidze.gera@gmail.com>
2023-10-08 17:08:02 +0300

    Finally, merged

--- ad_generator/run.sh
+++ ad_generator/run.sh
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 sudo python3 main_flask.py &
 sudo python3 loopcycle.py &
-=======
-sudo python3 main_flask.py
-sudo python3 loopcycle.py
->>>>>>> c570b74ba19c6cb88c861d9757b1bf8ee81814c1
